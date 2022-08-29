35c59dc622956d38a08ceb9beb28e110fc4b2bc0
bobjwalker <bob.walker@octopus.com>
2022-08-29 14:24:11 -0500

    Adding the powershell for the step template

---
+++ Scripts/Episode16_AutoApproveStepTemplate.ps1
@@ -0,0 +1,71 @@
+$CommandsRequireApproval = $OctopusParameters["Databases.AutoApprove.CommandsRequireApproval"]
+$CommandsIndicatingChange = $OctopusParameters["Databases.AutoApprove.CommandsIndicatingChange"]
+$ReportPath = $OctopusParameters["Databases.AutoApproval.ReportPath"]
+
+Write-Host "Commands requiring approval: $CommandsRequireApproval"
+Write-Host "Octopus indicating changes: $CommandsIndicatingChange"
+Write-Host "Report Path: $ReportPath"
+
+$ApprovalRequired = $false
+$HasDatabaseChanges = $false
+
+$fileContent = Get-Content -path $ReportPath
+
+$commandListToCheck = $CommandsRequireApproval -split ","
+$commandListForChanges = $CommandsIndicatingChange -split ","
+foreach ($sqlFile in $fileListToCheck)
+{    
+    Write-Host "Looping through all commands requiring approval for $ReportPath"
+	foreach ($command in $commandListToCheck)
+    {
+    	Write-Host "Checking $ReportPath for command $command"
+    	$foundCommand = $fileContent -match "$command"
+    
+    	if ($foundCommand)
+        {
+        	Write-Highlight "$ReportPath has the command '$command'"
+            $ApprovalRequired = $true
+            $HasDatabaseChanges = $true
+        }
+    }
+
+    if ($ApprovalRequired -eq $true)
+    {
+        Write-Host "Approval is required, so there has to be changes, skipping over the changes check"
+        break
+    }
+
+    Write-Host "Looping through all commands indicating changes for $reportPath"
+	foreach ($command in $commandListForChanges)
+    {
+    	Write-Host "Checking $ReportPath for command $command"
+    	$foundCommand = $fileContent -match "$command"
+    
+    	if ($foundCommand)
+        {
+        	Write-Highlight "$ReportPath has the command '$command'"
+            $HasDatabaseChanges = $true
+        }
+    }
+} 
+
+if ($approvalRequired -eq $false)
+{
+	Write-Highlight "All scripts look good"
+}
+else
+{
+	Write-Highlight "One of the specific commands we look for has been found"
+}
+
+if ($HasDatabaseChanges -eq $false)
+{
+    Write-Highlight "No database changes were detected"
+}
+else
+{
+    Write-Highlight "Database changes have been detected"
+}
+
+Set-OctopusVariable -name "ApprovalRequired" -value $ApprovalRequired
+Set-OctopusVariable -name "HasDatabaseChanges" -value $HasDatabaseChanges
